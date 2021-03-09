72530a91ef875175850b2b2f2768c1cda4a6e3ff
William Ford <wford1.home@gmail.com>
2021-03-09 15:41:16 +0900

    3rd major commit, core functionality is in place

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+//Array that the object is pushed to, this is then looped and each constructor displayed
 let myLibrary = [];
 
+// Object and constructor variables
 class Book {
 constructor(title, author, pages) {
         this.title = title;
@@ -24,20 +26,52 @@ const theHobbit = new Book('The Hobbit','J.R.R. Tolkien','295 pages')
         displayLibrary();
       }
     
+    //Display the form, function is called from HTML 'add book' button
 
-      // Click sumbit button and run the fucntion addBookToLibrary
-    document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById("submit").addEventListener("click", addBookToLibrary);
-    });
-    
+    function displayForm() {
+        const formDiv= document.querySelector('#form_container');
+
+        const formBook = document.createElement('form');
+        formBook.id = 'form';
+        formBook.name = 'form';
+        formDiv.appendChild(formBook);
+
+        function appendInput(type, id, labelText) {
+            const label = document.createElement('label');
+            label.htmlFor = id;
+            label.textContent = labelText;
+            formBook.appendChild(label);
+          
+            const input = document.createElement('input');
+            input.type = type;
+            input.id = id;
+            input.name = id;
+            formBook.appendChild(input);
+        
+            const br = document.createElement('br');
+            formBook.appendChild(br);
+          }
+        
+          appendInput("text", "title", "Title:", );
+          appendInput("text", "author", "Author:", );
+          appendInput("number", "pages", "# of pages:",);
+          
+        //Submit button of form 
+          const submit = document.createElement('button');
+          submit.id = 'submit';
+          submit.textContent = 'Submit';
+          submit.addEventListener('click', addBookToLibrary);
+          formBook.appendChild(submit);
+    }
     // Display the library
 
     function displayLibrary() {
         const container = document.querySelector('#library_container');
         container.textContent = '';
 
+        //My library is looped, table created and text content taken from the constructors
         myLibrary.forEach((book, index) => {
-            const bookPanel = document.createElement('div');
+            const bookPanel = document.createElement('table');
             bookPanel.classList.add('book');
             bookPanel.dataset.index = index;
             container.appendChild(bookPanel);
@@ -47,7 +81,7 @@ const theHobbit = new Book('The Hobbit','J.R.R. Tolkien','295 pages')
             bookPanel.appendChild(titleText);
             
             const authorText = document.createElement('p');
-            authorText.textContent = "By " + book.author;
+            authorText.textContent = book.author;
             bookPanel.appendChild(authorText);
 
             const pages = document.createElement('p');
